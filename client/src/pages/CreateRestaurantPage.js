77f82c2697050e8953bf3449322cd5a86a4e842d
aleshkaaaa <82362134+aleshkaaaaa@users.noreply.github.com>
2024-03-02 20:33:08 +0300

    build 1.2

--- client/src/pages/CreateRestaurantPage.js
+++ client/src/pages/CreateRestaurantPage.js
@@ -12,7 +12,7 @@ export default function CreateRestaurantPage() {
     async function createNewRestaurant(ev) {
         ev.preventDefault();
         try {
-            const response = await fetch('http://localhost:4000/post', {
+            const response = await fetch(process.env.REACT_APP_API_URL, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
